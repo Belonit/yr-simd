65e40ef35604deeabfc81ab366201ab28ffa52c4
Belonit <54427022+Belonit@users.noreply.github.com>
2026-05-17 03:33:34 +0300

    Removed dead code

--- src/Utilities/Simd.h
+++ src/Utilities/Simd.h
@@ -26,33 +26,6 @@ public:
 	static const char* GetLevelName(Level level);
 	static Level ParseLevel(const char* pValue, Level defaultLevel);
 
-	template <typename T>
-	static T Dispatch(
-		T scalarPath,
-		T sse2Path,
-		T avx2Path = nullptr,
-		T avx512Path = nullptr)
-	{
-		switch (GetCurrentLevel())
-		{
-		case Level::AVX512:
-			if (avx512Path)
-				return avx512Path;
-			[[fallthrough]];
-		case Level::AVX2:
-			if (avx2Path)
-				return avx2Path;
-			[[fallthrough]];
-		case Level::SSE2:
-			if (sse2Path)
-				return sse2Path;
-			[[fallthrough]];
-		default:
-		case Level::Scalar:
-			return scalarPath;
-		}
-	}
-
 private:
 	static Level DetectBestLevel();
 	static Level ClampLevel(Level level, Level maxLevel);
